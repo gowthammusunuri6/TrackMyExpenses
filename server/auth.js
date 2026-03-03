8b63f5e94b6c05ec02ac369f579d893e185898c3
Gowtham <gowthammusunuri6@gmail.com>
2026-03-03 19:45:43 +0530

    Replaced bcrypt with bcryptjs

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@
 // ==========================================
 
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
